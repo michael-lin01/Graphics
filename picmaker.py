22eca5337a395f90fd6a7ceafebe62e7697d7de0
Michael Lin <michaellin01@gmail.com>
2020-01-31 22:37:58 -0500

    picmaker

--- picmaker.py
+++ picmaker.py
@@ -1,13 +1,12 @@
 if __name__ == "__main__":
     f = open('image.ppm','w')
-    f.write('P3 500 500 255\n')
-    r = 0
-    g = 0
-    b = 255
+    f.write('P3 500 500 500\n')
+
+    r = 500
     for row in range(500):
+        g = row
         for col in range(500):
+            b = col
             color = '%d %d %d ' % (r,g,b)
             f.write(color)
-            g = (g+1)%256
-            r = (r+1)%256
     f.close()
